bbc82df34dc32247fba871c815e1948d76fc5c98
James L <howpigcanfly@gmail.com>
2016-01-18 13:48:23 +0800

    add jni unit

---
+++ app/src/main/java/abc/bookcl/test01/jni/endless.c
@@ -0,0 +1,5 @@
+//
+// Created by NUSW on 2016/1/18.
+//
+
+#include "endless.h"
---
+++ app/src/main/java/abc/bookcl/test01/jni/endless.h
@@ -0,0 +1,8 @@
+//
+// Created by NUSW on 2016/1/18.
+//
+
+#ifndef TEST01_ENDLESS_H
+#define TEST01_ENDLESS_H
+
+#endif //TEST01_ENDLESS_H
