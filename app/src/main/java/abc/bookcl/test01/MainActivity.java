1b9d3f9c2dac77d4f78f5aa9db84c1dcac06fc53
James L <howpigcanfly@gmail.com>
2016-01-18 15:08:37 +0800

    set main activity load jni

--- app/src/main/java/abc/bookcl/test01/MainActivity.java
+++ app/src/main/java/abc/bookcl/test01/MainActivity.java
@@ -9,15 +9,26 @@ import abc.bookcl.test01.TSUnit.UnitMain;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
+    public static native String getStringFormC();
+
+    static {
+        System.loadLibrary("xyz");    //defaultConfig.ndk.moduleName
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("TAG", "--------------------> " + getStringFormC());
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setText(getStringFormC());
+        /*
         Log.d("TAG", "--------------------> " + UnitMain.get_java_str("=="));
         Log.d("TAG","--------------------> " + UnitMain.getStringFormC());
         textView = (TextView) findViewById(R.id.textView);
         //textView.setText(UnitMain.getStringFormC());
         textView.setText(UnitMain.get_java_str(null) + UnitMain.getStringFormC());
+        */
     }
 }
--- app/src/main/jni/endless.c
+++ app/src/main/jni/endless.c
@@ -5,7 +5,7 @@
 #include "endless.h"
 
 
-JNIEXPORT jstring JNICALL Java_abc_bookcl_test01_TSUnit_UnitMain_getStringFormC
+JNIEXPORT jstring JNICALL Java_abc_bookcl_test01_MainActivity_getStringFormC
         (JNIEnv *env, jobject obj){
     return (*env)->NewStringUTF(env,"Test By JamesL");
 }
