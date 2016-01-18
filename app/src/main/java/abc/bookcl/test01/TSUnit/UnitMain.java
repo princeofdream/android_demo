56832709806b207f39859089afc1deb01ebd1911
James L <howpigcanfly@gmail.com>
2016-01-18 14:49:54 +0800

    add java function

--- app/src/main/java/abc/bookcl/test01/TSUnit/UnitMain.java
+++ app/src/main/java/abc/bookcl/test01/TSUnit/UnitMain.java
@@ -4,4 +4,12 @@ package abc.bookcl.test01.TSUnit;
  * Created by NUSW on 2016/1/18.
  */
 public class UnitMain {
+    //public static native String getStringFormC();
+
+    static {
+        //System.loadLibrary("test01");    //defaultConfig.ndk.moduleName
+    }
+    public static String get_java_str(String msg) {
+        return "String from java";
+    }
 }
--- app/src/main/jni/endless.c
+++ app/src/main/jni/endless.c
@@ -3,3 +3,9 @@
 //
 
 #include "endless.h"
+
+
+JNIEXPORT jstring JNICALL Java_abc_bookcl_test01_TSUnit_UnitMain_getStringFormC
+        (JNIEnv *env, jobject obj){
+    return (*env)->NewStringUTF(env,"Test By JamesL");
+}
