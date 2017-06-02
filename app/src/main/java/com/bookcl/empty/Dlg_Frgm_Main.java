686189d0b7eb358cba41ae28ce808b7fae3e5948
W James <wingodr@outlook.com>
2017-06-02 17:30:47 +0800

    Add DialogFragment Demo

---
+++ app/src/main/java/com/bookcl/empty/Dlg_Frgm_Main.java
@@ -0,0 +1,81 @@
+package com.bookcl.empty;
+
+import android.app.DialogFragment ;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+/**
+ * Created by lijin on 2017/6/2.
+ */
+
+public class Dlg_Frgm_Main extends DialogFragment {
+
+    int mNum;
+
+    public static Dlg_Frgm_Main newInstance(int num){
+        Dlg_Frgm_Main mdlg = new Dlg_Frgm_Main();
+        // Supply num input as an argument.
+        Bundle args = new Bundle();
+        args.putInt("num", num);
+        mdlg.setArguments(args);
+
+        return mdlg;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mNum = getArguments().getInt("num");
+
+        Log.d("JamesL","get number -->"+mNum);
+
+        // Pick a style based on the num.
+        int style = DialogFragment.STYLE_NORMAL, theme = 0;
+        switch ((mNum-1)%6) {
+            case 1: style = DialogFragment.STYLE_NO_TITLE; break;
+            case 2: style = DialogFragment.STYLE_NO_FRAME; break;
+            case 3: style = DialogFragment.STYLE_NO_INPUT; break;
+            case 4: style = DialogFragment.STYLE_NORMAL; break;
+            case 5: style = DialogFragment.STYLE_NORMAL; break;
+            case 6: style = DialogFragment.STYLE_NO_TITLE; break;
+            case 7: style = DialogFragment.STYLE_NO_FRAME; break;
+            case 8: style = DialogFragment.STYLE_NORMAL; break;
+        }
+        switch ((mNum-1)%6) {
+            case 4: theme = android.R.style.Theme_Holo; break;
+            case 5: theme = android.R.style.Theme_Holo_Light_Dialog; break;
+            case 6: theme = android.R.style.Theme_Holo_Light; break;
+            case 7: theme = android.R.style.Theme_Holo_Light_Panel; break;
+            case 8: theme = android.R.style.Theme_Holo_Light; break;
+        }
+        setStyle(style, theme);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.frgm_dlg_main_ly, container, false);
+        View tv = v.findViewById(R.id.dlg_info);
+        ((TextView)tv).setText("Dialog #" + mNum + ": using style ");
+
+
+        // Watch for button clicks.
+        Button button = (Button)v.findViewById(R.id.show);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // When button is clicked, call up to owning activity.
+                Log.d("JamesL","------> 006, get num" + mNum);
+            }
+        });
+
+        return v;
+    }
+
+}
--- app/src/main/java/com/bookcl/empty/MainActivity.java
+++ app/src/main/java/com/bookcl/empty/MainActivity.java
@@ -1,19 +1,24 @@
 package com.bookcl.empty;
 
+import android.app.DialogFragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.app.Fragment;
 import android.app.ListFragment;
+import android.content.Context;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
+    int mStackLevel=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,14 +42,39 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
         Button frag_gd_man_bt2 = (Button) this.findViewById(R.id.gd_main_bt2);
         frag_gd_man_bt2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d("JamesL", "--------------> 002 ");
+                showDialog();
             }
         });
 
+        Log.d("JamesL", "--------------> 004 ");
+
+    }
+
+    void showDialog() {
+        mStackLevel++;
+
+        // DialogFragment.show() will take care of adding the fragment
+        // in a transaction.  We also want to remove any currently showing
+        // dialog, so make our own transaction and take care of that here.
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+
+        /*
+        Fragment prev = getFragmentManager().findFragmentByTag("f_layout");
+        if (prev != null) {
+            ft.remove(prev);
+        }
+        ft.addToBackStack(null);
+        */
+
+        // Create and show the dialog.
+        DialogFragment newFragment = Dlg_Frgm_Main.newInstance(mStackLevel);
+        newFragment.show(ft, "f_layout");
     }
 
 
