0b4f89f039f78adca3aa854485b4903fa607f8c2
W James <wingodr@outlook.com>
2017-06-06 16:59:06 +0800

    Add drawable and info array

--- app/src/main/java/com/bookcl/empty/MainActivity.java
+++ app/src/main/java/com/bookcl/empty/MainActivity.java
@@ -18,13 +18,26 @@ import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
-    int mStackLevel=0;
+    private int mStackLevel=0;
+    private int minfocount=0;
+    String TAG = "[JamesL]-Main";
+
+    InfoArray[] mInfoAray = new InfoArray[] {
+            new InfoArray(R.string.info_00, true),
+            new InfoArray(R.string.info_01, true),
+            new InfoArray(R.string.info_02, true),
+            new InfoArray(R.string.info_03, true),
+            new InfoArray(R.string.info_04, true),
+            new InfoArray(R.string.info_05, true),
+            new InfoArray(R.string.info_06, true),
+            new InfoArray(R.string.info_07, true)
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("JamesL","onCreate");
+        Log.d(TAG,"onCreate");
 
     }
 
@@ -32,29 +45,79 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d("JamesL","onResume");
+        Log.d(TAG,"onResume");
+
+        final Button m_prev_bt = (Button) this.findViewById(R.id.main_prev);
+        final Button m_next_bt = (Button) this.findViewById(R.id.main_next);
+
+        final TextView mTxInfo = (TextView) findViewById(R.id.txinfo);
+
+        if(minfocount == 0)
+            m_prev_bt.setEnabled(false);
+
+        mTxInfo.setText(mInfoAray[minfocount].getInfo());
+
+        mTxInfo.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                minfocount++;
+                if(minfocount == 7 )
+                    m_next_bt.setEnabled(false);
+                else if( minfocount > 0)
+                    m_prev_bt.setEnabled(true);
+
+                if (minfocount > 7) {
+                    minfocount = 7;
+                    return;
+                }
+
+                mTxInfo.setText(mInfoAray[minfocount].getInfo());
+                Log.d(TAG,"click text!");
+            }
+        });
+
+        m_prev_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                minfocount--;
+                if(minfocount == 0)
+                    m_prev_bt.setEnabled(false);
+                else if( minfocount< 7)
+                    m_next_bt.setEnabled(true);
+                mTxInfo.setText(mInfoAray[minfocount].getInfo());
+                Log.d(TAG,"click prev button!");
+            }
+        });
+
+
+        if(minfocount == 7 )
+            m_next_bt.setEnabled(false);
+        m_next_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                minfocount++;
+                if(minfocount == 7 )
+                    m_next_bt.setEnabled(false);
+                else if( minfocount > 0)
+                    m_prev_bt.setEnabled(true);
+                mTxInfo.setText(mInfoAray[minfocount].getInfo());
+                Log.d(TAG,"click next button!");
+            }
+        });
+
+
 
         Button frag_gd_man_bt = (Button) this.findViewById(R.id.gd_main_bt);
         frag_gd_man_bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("JamesL", "--------------> 001 ");
-            }
-        });
-
-
-        Button frag_gd_man_bt2 = (Button) this.findViewById(R.id.gd_main_bt2);
-        frag_gd_man_bt2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d("JamesL", "--------------> 002 ");
                 showDialog();
             }
         });
 
-        Log.d("JamesL", "--------------> 004 ");
 
     }
+
 
     void showDialog() {
         mStackLevel++;
@@ -63,19 +126,12 @@ public class MainActivity extends AppCompatActivity {
         // in a transaction.  We also want to remove any currently showing
         // dialog, so make our own transaction and take care of that here.
         FragmentTransaction ft = getFragmentManager().beginTransaction();
-
-        /*
-        Fragment prev = getFragmentManager().findFragmentByTag("f_layout");
-        if (prev != null) {
-            ft.remove(prev);
-        }
-        ft.addToBackStack(null);
-        */
-
+        
         // Create and show the dialog.
         DialogFragment newFragment = Dlg_Frgm_Main.newInstance(mStackLevel);
         newFragment.show(ft, "f_layout");
     }
+
 
 
 }
