50f572fb6edde97df67d21ce2d9ed6f2b05e177d
W James <wingodr@outlook.com>
2017-06-16 12:45:39 +0800

    Add third part lib compile option

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -5,6 +5,10 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := xyz
 LOCAL_SRC_FILES := endless.c
 
+## for third part so libraries
+#LOCAL_C_INCLUDES := $(LOCAL_PATH)/include $(LOCAL_PATH)/../jniLibs/include
+#LOCAL_LDFLAGS := -L$(LOCAL_PATH)/../jniLibs/armeabi-v7a  -lx264
+
 include $(BUILD_SHARED_LIBRARY)
 
 
