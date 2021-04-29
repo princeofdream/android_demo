f408099affabc8e3437ee4a15509844491a91b95
James Lee <princeofdream@outlook.com>
2021-04-29 10:38:35 +0800

    serial: write data to serial okay

---
+++ serial/android_serial_api/build/generated/source/buildConfig/debug/android_serialport_api/sample/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package android_serialport_api.sample;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "android_serialport_api.sample";
+  public static final String BUILD_TYPE = "debug";
+}
--- serial/app/src/main/java/com/bookcl/serial/MainActivity.java
+++ serial/app/src/main/java/com/bookcl/serial/MainActivity.java
@@ -2,7 +2,17 @@ package com.bookcl.serial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Arrays;
+
+import android_serialport_api.SerialPort;
+import android_serialport_api.SerialPortFinder;
+import android_serialport_api.sample.Sending01010101Activity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +20,12 @@ public class MainActivity extends AppCompatActivity {
     static {
         System.loadLibrary("serial_jni");
     }
+
+    public SerialPortFinder mSerialPortFinder = new SerialPortFinder();
+    private SerialPort mSerialPort = null;
+    MainActivity.SendingThread mSendingThread;
+    byte[] mBuffer;
+    protected OutputStream mOutputStream;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +35,66 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        try {
+            mSerialPort = new SerialPort(new File("/dev/ttyUSB0"), 115200, 0);
+            mOutputStream = mSerialPort.getOutputStream();
+            mBuffer = new byte[4];
+//            Arrays.fill(mBuffer, (byte) 0x55);
+            if (mSerialPort != null) {
+                mSendingThread = new MainActivity.SendingThread();
+                mSendingThread.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class SendingThread extends Thread {
+        @Override
+        public void run() {
+            int i0=0;
+            int val = 0x33;
+
+            while (!isInterrupted()) {
+                mBuffer[0] = 0x31;
+                mBuffer[1] = 0x32;
+                mBuffer[2] = 0x33;
+//                mBuffer[3] = 0x0D;
+                val = 0x33 + i0;
+                mBuffer[2] = (byte)val;
+
+                try {
+                    if (mOutputStream != null) {
+                        Log.d("byJames", "fill data.........");
+                        mOutputStream.write(mBuffer);
+                    } else {
+                        Log.d("byJames", "abort.........");
+                        return;
+                    }
+                    Thread.sleep(1000);
+                } catch (IOException | InterruptedException e) {
+                    e.printStackTrace();
+                    return;
+                }
+                i0++;
+                if (i0 > 6)
+                    break;
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mSerialPort != null) {
+            mSerialPort.close();
+            mSerialPort = null;
+        }
     }
 
     /**
